117da27f9ee5068aad6fb5f05bcde4a7dedeeaff
Jonathan Barrow <jonbarrow1998@gmail.com>
2021-06-21 19:00:12 -0400

    removed ResponseStream

--- themesmoe.py
+++ themesmoe.py
@@ -1,7 +1,7 @@
 # Download series opening and endings
 
 import requests
-from stream_response import ResponseStream
+import os
 from pathlib import Path
 from pydub import AudioSegment
 
@@ -17,7 +17,9 @@ def get_themes(mal_id):
 		theme_type = theme["themeType"]
 		theme_url = theme["mirror"]["mirrorURL"]
 		file_name = theme_url.rsplit('/', 1)[1]
+
 		mp3_file_name = f"{Path(file_name).stem}.mp3"
+		
 		theme_folder = None
 
 		if "OP" in theme_type:
@@ -26,10 +28,16 @@ def get_themes(mal_id):
 			theme_folder = "./endings"
 	
 		mp3_path = f"{theme_folder}/{mp3_file_name}"
+		video_path = f"{theme_folder}/{file_name}"
 
 		headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4521.0 Safari/537.36 Edg/93.0.910.5"}
 		response = requests.get(theme_url, allow_redirects=True, headers=headers, stream=True)
-		stream = ResponseStream(response.iter_content(1024*1024))
-		AudioSegment.from_file(stream).export(mp3_path, format="mp3")
+
+		video_file = open(video_path, "wb")
+		for chunk in response.iter_content(chunk_size=1024*1024): 
+			video_file.write(chunk)
+
+		AudioSegment.from_file(video_path).export(mp3_path, format="mp3")
+		os.remove(video_path)
 	
 	return True
